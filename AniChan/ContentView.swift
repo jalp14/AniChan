fe7706f7bc596c9f8d78edade96a5d829f725331
Jalp <jalpdesai@icloud.com>
2019-09-25 13:38:53 +0100

    Initial Commit

---
+++ AniChan/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AniChan
+//
+//  Created by Jalp on 25/09/2019.
+//  Copyright © 2019 JDC0rp. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
